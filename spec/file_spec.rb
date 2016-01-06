9f7314c7475eee602178d485c65eb40d23cdf956
Raffael Schmid <raffael@yux.ch>
2016-01-06 10:19:49 +0100

    Add unit tests for LSVplus::File

--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe LSVplus::File do
       lsv_identification: 'YOLO1',
     }
   end
+  let(:instance) { one_time_instance }
 
   def one_time_instance
     LSVplus::File.new(attributes)
@@ -46,6 +47,52 @@ RSpec.describe LSVplus::File do
         attributes[:lsv_identification] = 'ASD12'
         expect { one_time_instance }.to_not raise_error
       end
+    end
+  end
+
+  describe '#add_record' do
+    it 'adds a record to #records' do
+      expect { instance.add_record('a') }.to change { instance.records }.from([]).to(%w(a))
+    end
+  end
+
+  describe '#to_s' do
+    it 'concatenates the records and the total record' do
+      expect(instance).to receive(:records_as_string).and_return('records').ordered
+      expect(instance).to receive(:total_record).and_return('total_record').ordered
+      expect(instance.to_s).to eq('recordstotal_record')
+    end
+  end
+
+  describe '#records_as_string' do
+    it 'calls RecordFormatter for each record' do
+      expect(LSVplus::RecordFormatter).to receive(:call).with(instance, 'record1', 1).once
+      expect(LSVplus::RecordFormatter).to receive(:call).with(instance, 'record2', 2).once
+      instance.add_record 'record1'
+      instance.add_record 'record2'
+      instance.records_as_string
+    end
+
+    it 'concatenates the records' do
+      expect(LSVplus::RecordFormatter).to receive(:call).and_return('a', 'b').twice
+      instance.add_record 'record1'
+      instance.add_record 'record2'
+      expect(instance.records_as_string).to eq('ab')
+    end
+  end
+
+  describe '#total_record' do
+    it 'calls TotalRecordFormatter' do
+      expect(LSVplus::TotalRecordFormatter).to receive(:call).with(instance)
+      instance.total_record
+    end
+  end
+
+  describe '#total' do
+    it 'sums record.amount' do
+      instance.add_record OpenStruct.new(amount: BigDecimal.new('0.9'))
+      instance.add_record OpenStruct.new(amount: 99)
+      expect(instance.total).to eq(BigDecimal.new('99.9'))
     end
   end
 end
